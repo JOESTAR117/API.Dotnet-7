162c4db9e050e356bf0925f9676b8a6221c69d9a
Joestar117 <juniorrios1231@gmail.com>
2023-06-26 23:11:32 -0300

    feat: DbContext and mapping the person class

---
+++ API.Dotnet-7.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Dotnet_7.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Dotnet_7.Infra.Data.Context
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{ }
+
+		public DbSet<Person> People { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+		}
+	}
+}
---
+++ API.Dotnet-7.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,31 @@
+﻿using API.Dotnet_7.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace API.Dotnet_7.Infra.Data.Maps
+{
+	public class PersonMap : IEntityTypeConfiguration<Person>
+	{
+		public void Configure(EntityTypeBuilder<Person> builder)
+		{
+			builder.ToTable("Pessoa");
+			builder.HasKey(x => x.Id);
+			builder.Property(x => x.Id)
+				.HasColumnName("IdPessoa")
+				.UseIdentityColumn();
+
+			builder.Property(x => x.Document)
+				.HasColumnName("Documento");
+
+			builder.Property(x => x.Name)
+				.HasColumnName("Nome");
+
+			builder.Property(x => x.Phone)
+				.HasColumnName("Celular");
+
+			builder.HasMany(x => x.Purchases)
+				.WithOne(p => p.Person)
+				.HasForeignKey(x => x.PersonId);
+		}
+	}
+}
