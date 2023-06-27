6d5d45b558df6ba2d83dac4cbf7f7214f154fdf1
Joestar117 <juniorrios1231@gmail.com>
2023-06-26 23:53:16 -0300

    feat: mapping the product and purchase class

--- API.Dotnet-7.Domain/Entities/Product.cs
+++ API.Dotnet-7.Domain/Entities/Product.cs
@@ -18,18 +18,18 @@ namespace API.Dotnet_7.Domain.Entities
 
 		public int Id { get; private set; }
 		public string Name { get; private set; }
-		public string CodeErp { get; private set; }
+		public string CodErp { get; private set; }
 		public decimal Price { get; private set; }
 		public ICollection<Purchase> Purchases { get; set; }
 
-		private void Validation(string name, string codeErp, decimal price)
+		private void Validation(string name, string codErp, decimal price)
 		{
 			DomainValidationException.When(string.IsNullOrEmpty(name), "Name must be informed");
-			DomainValidationException.When(string.IsNullOrEmpty(codeErp), "CodeErp must be informed");
+			DomainValidationException.When(string.IsNullOrEmpty(codErp), "CodErp must be informed");
 			DomainValidationException.When(price < 0, "price must be greater than zero");
 
 			Name = name;
-			CodeErp = codeErp;
+			CodErp = codErp;
 			Price = price;
 		}
 
